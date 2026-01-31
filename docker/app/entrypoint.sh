f9933b3ecf2132c90e6ae76b51ef7f5508c20ca7
codest40 <generalcatmouse@gmail.com>
2026-01-31 13:46:08 +0100

    New Commit on 2026_01_31_13_46_08

--- docker/app/entrypoint.sh
+++ docker/app/entrypoint.sh
@@ -9,6 +9,10 @@ python wait_for_db.py
 
 # Run Alembic only if RUN_MIGRATIONS is set to true
 echo "Detected Env RUN_MIGRATIONS: $RUN_MIGRATIONS"
+echo "Detected Env DATABASE_URL: $DATABASE_URL"
+echo "Detected Env OPENWEATHER KEY: $OPENWEATHER_API_KEY"
+echo "Detected Env CONTAINER_PORT: $CONTAINER_PORT"
+echo "Detected Env ACTIVE_PORT: $active_port"
 
 if [ "${RUN_MIGRATIONS,,}" = "true" ]; then
     echo "[START(Entry) $timer] Running Alembic migrations..."
