2fdb0c1c4ca0bca759c477f32bebddeb98877792
codest40 <generalcatmouse@gmail.com>
2026-02-02 00:21:59 +0100

    New Commit on 2026_02_02_00_21_59

--- scripts/update-inventory.sh
+++ scripts/update-inventory.sh
@@ -95,7 +95,7 @@ else
 all:
   hosts:
     edgepaas:
-      ansible_host: "$EC2_IP"
+      ansible_host: $EC2_IP
       ansible_user: ec2-user
       ansible_python_interpreter: /usr/bin/python3
       ansible_ssh_args: ' -o StrictHostKeyChecking=no'
