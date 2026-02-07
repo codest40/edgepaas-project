37f72dfe212dbc8cc3398a80a3015201e07fd2a4
codest40 <generalcatmouse@gmail.com>
2026-02-07 11:43:31 +0100

    AUTO-COMMIT on 2026-02-07 11:43:31

--- docker/app/reset_alembic.sh
+++ docker/app/reset_alembic.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 set -euo pipefail
 
-export PATH=$PATH:/usr/pgsql-15/bin
-
 echo "[ALEMBIC] Resetting migrations..."
 
-# Remove Alembic versions folder (for Test_DB)
+# Remove Alembic versions folder
 rm -rf /app/alembic/versions/*
 
-# Reset the DB (Test)
- psql $DATABASE_URL -c "DROP SCHEMA public CASCADE; CREATE SCHEMA public;"
+# Full path to ensure psql is found
+/usr/pgsql-15/bin/psql $DATABASE_URL -c "DROP SCHEMA public CASCADE; CREATE SCHEMA public;"
 
 echo "[ALEMBIC] Alembic versions cleared. ✅"
--- docker/app/test.py
+++ docker/app/test.py
@@ -2,10 +2,12 @@ import os, sys
 from dotenv import load_dotenv
 load_dotenv()
 from wait_for_db import DATABASE_URL
+import psycopg2
 
-if not DATABASE_URL:
-  print("No DB URL Found")
-  sys.exit()
-else:
-  print(f"DB Found! : {DATABASE_URL}")
+try:
+    conn = psycopg2.connect(add_sslmode(DATABASE_URL))
+    conn.close()
+    print("[SUCCESS] SSL connection to Render DB works!")
+except Exception as e:
+    print(f"[FAIL] SSL connection failed: {e}")
 
