a3ac0c7e523392a4a5850b7ec60ca6e527578761
codest40 <generalcatmouse@gmail.com>
2026-02-07 10:41:59 +0100

    AUTO-COMMIT on 2026-02-07 10:41:59

--- docker/app/reset_alembic.sh
+++ docker/app/reset_alembic.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -euo pipefail
+export PATH=$PATH:/usr/pgsql-15/bin
 
 echo "[ALEMBIC] Resetting migrations..."
 
@@ -9,4 +10,4 @@ rm -rf /app/alembic/versions/*
 # Reset the DB (Test)
  psql $DATABASE_URL -c "DROP SCHEMA public CASCADE; CREATE SCHEMA public;"
 
-echo "[ALEMBIC] Alembic versions cleared."
+echo "[ALEMBIC] Alembic versions cleared. ✅"
