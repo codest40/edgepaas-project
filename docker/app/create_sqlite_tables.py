c4f36c994fd80effd54cafd297b58607e61c14ad
codest40 <generalcatmouse@gmail.com>
2026-02-09 11:48:46 +0100

    AUTO-COMMIT on 2026-02-09 11:48:46

--- docker/app/create_sqlite_tables.py
+++ docker/app/create_sqlite_tables.py
@@ -1,6 +1,7 @@
-from models import Base
-from db import engine
+# create_sqlite_tables.py
 import os
+from db import Base, engine
+import models
 
 if os.environ.get("USE_SQLITE", "false") == "true" or os.environ.get("FINAL_DB_MODE") == "sqlite_only":
     print("[INFO] Creating tables for SQLite...")
