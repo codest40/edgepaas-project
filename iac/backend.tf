37359f2836329565abcdf3d2a64a458ebda66a8c
codest40 <generalcatmouse@gmail.com>
2026-04-24 15:52:54 +0100

    AUTO-COMMIT on 2026-04-24 15:52:54

--- iac/backend.tf
+++ iac/backend.tf
@@ -1,10 +1,10 @@
 # Remote backend for shared state, locking, and safe collaboration
 terraform {
   backend "s3" {
-    bucket         = "edgepaas-backend-tf"
-    key            = "edgepaas/terraform.tfstate"
+    bucket         = "kubdeploy-backend-tf"
+    key            = "kubdeploy/terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "edgepaas-lock-tf"
+    dynamodb_table = "kubdeploy-lock-tf"
     encrypt        = true
   }
 }
